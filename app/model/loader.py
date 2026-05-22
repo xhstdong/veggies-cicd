a7a5e8aafc094616228a5c06d4d020ef9c4f6e25
xhstdong <stdong@hotmail.com>
2026-05-22 12:23:50 -0400

    Revert "bug fix: weights_only=False"
    This reverts commit f686235ea107cdbdee5eb897f1f7152d918cd890.
    
    Revert changes to weights_only

--- app/model/loader.py
+++ app/model/loader.py
@@ -8,7 +8,7 @@ def initialize_model(num_classes):
     # initalize resnet18 model
     # model weights will be loaded separately
     # generate a final fully connected layer for tailored classification
-    model = models.resnet18(weights=None)
+    model = models.resnet18(pretrained=False)
     num_ftrs = model.fc.in_features
     model.fc = nn.Linear(num_ftrs, num_classes)
     return model
@@ -16,7 +16,7 @@ def initialize_model(num_classes):
 def load_model(weights_path: str, device: torch.device):
     # load model weights
     model = initialize_model(NUM_CLASSES)
-    model.load_state_dict(torch.load(weights_path, map_location=device, weights_only=False))
+    model.load_state_dict(torch.load(weights_path, map_location=device))
     model.to(device)
     model.eval()
     return model
